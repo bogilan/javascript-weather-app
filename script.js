7566f114435dbc296ecff255cd7147f0ddf74862
bogilan <bogdan.lakicevic@gmail.com>
2023-02-22 23:28:04 +0100

    Updated JavaScript code with Hourly and 7 Days Forecast

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
 const weatherForecastURL = 'https://api.weatherapi.com/v1/forecast.json?key=9415c5aecbad4810a78201126232102&q=';
 
-const searchCity = document.getElementById('search-city');
+const searchCityInput = document.getElementById('search-city');
 const searchBtn = document.querySelector('.search-btn');
 
-const cityName = document.querySelector('.location-name');
-const cityTemp = document.querySelector('.current-temp');
-const weatherIcon = document.querySelector('.current-weather-icon');
-const weatherCondition = document.querySelector('.current-weather-desc');
-const datetime = document.querySelector('.date-time');
+const cityNameEl = document.querySelector('.location-name');
+const cityTempEl = document.querySelector('.current-temp');
+const weatherIconEl = document.querySelector('.current-weather-icon');
+const weatherConditionEl = document.querySelector('.current-weather-desc');
+const datetimeEl = document.querySelector('.date-time');
+const airQualityIndexEl = document.querySelector('.air-quality-index');
+
+const currentForecastEl = document.querySelector('.current-forecast');
+const daysForecastEl = document.querySelector('.days-forecast');
+const hourlyForecastEl = document.querySelector('.hourly-forecast');
 
 document.addEventListener("DOMContentLoaded", ()=> {
   getWeatherData('London');
 });
 
 searchBtn.addEventListener('click', ()=> {
-  getWeatherData(searchCity.value);
+  getWeatherData(searchCityInput.value);
 })
-
 
 async function getWeatherData(city) {
   try {
-      const res = await fetch(`${weatherForecastURL}${city}`);
+      const res = await fetch(`${weatherForecastURL}${city}&aqi=yes&days=8`);
       const data = await res.json();
 
-      cityName.textContent = data.location.name;
-      cityTemp.textContent = data.current.temp_c;
-      weatherIcon.src = data.current.condition.icon;
-      weatherCondition.textContent = data.current.condition.text;
-      datetime.textContent = data.location.localtime;
+      cityNameEl.textContent = data.location.name;
+      cityTempEl.innerHTML = `${data.current.temp_c} &#8451;`;
+      weatherIconEl.src = data.current.condition.icon;
+      weatherConditionEl.textContent = data.current.condition.text;
+      datetimeEl.textContent = data.location.localtime;
+      airQualityIndexEl.textContent = data.current.air_quality["us-epa-index"];
 
+      getHourlyForecast(data);
+
+      get7DayForecast(data);
 
   }
   catch(error) {
       alert('Failed to find, please try again.')
+  }
+}
+
+function getHourlyForecast(data) {
+  hourlyForecastEl.innerHTML = '<h2>Hourly Forecast</h2>';
+  const localtime = new Date(data.location.localtime);
+  const hour = localtime.getHours();
+  
+  for(let hourIndex = hour + 1; hourIndex < 24; hourIndex++) {
+    const hourData = data.forecast.forecastday[0].hour[hourIndex];
+    const { temp_c, condition: { text } } = hourData;
+    const hourHTML = `<div class="hour-forecast">
+                        <span class="hour">${hourIndex}:00</span>
+                        <span class="temp">${temp_c} &#8451;</span>
+                        <span class="condition">${text}</span>
+                      </div>`;
+    hourlyForecastEl.innerHTML += hourHTML;
+  }
+}
+
+function get7DayForecast(data) {
+  daysForecastEl.innerHTML = '<h2>7 Days Forecast</h2>';
+
+  for(let dayIndex = 1; dayIndex < 8; dayIndex++) {
+    const dayData = data.forecast.forecastday[dayIndex];
+    const { date, day: { avgtemp_c } } = dayData;
+    const dayHTML = `<div class="day">
+                        <span class="day-date">${date}</span>
+                        <span class="temp">${avgtemp_c} &#8451;</span>
+                      </div>`
+    daysForecastEl.innerHTML += dayHTML;
   }
 }
